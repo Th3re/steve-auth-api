27cf32fafd54c91f8b7e52ede8b1f18c00e22e81
Piotr Persona <persona.piotr@gmail.com>
2020-03-22 19:07:27 +0100

    Add refresh_token method to PermissionIssuer
    Signed-off-by: Piotr Persona <persona.piotr@gmail.com>

--- api/credentials/google_issuer.py
+++ api/credentials/google_issuer.py
@@ -15,11 +15,6 @@ class GoogleIssuer(PermissionIssuer):
         self.client_secret = google_configuration.client_secret
         self.client_id = google_configuration.client_id
 
-    @staticmethod
-    def __get_subject(id_token):
-        decoded_token = jwt.decode(id_token, verify=False, algorithms=['RS256'])
-        return decoded_token['sub']
-
     def get_credentials(self, code: str) -> Credentials:
         response = requests.post(url=self.request_url,
                                  data=dict(grant_type='authorization_code',
@@ -31,3 +26,20 @@ class GoogleIssuer(PermissionIssuer):
         LOG.debug(f'Authentication response: {data}')
         subject = self.__get_subject(data['id_token'])
         return Credentials(refresh_token=data['refresh_token'], user_id=subject)
+
+    @staticmethod
+    def __get_subject(id_token):
+        decoded_token = jwt.decode(id_token, verify=False, algorithms=['RS256'])
+        return decoded_token['sub']
+
+    def refresh_token(self, refresh_token) -> str:
+        response = requests.post(url=self.request_url,
+                                 data=dict(grant_type='refresh_token',
+                                           refresh_token=refresh_token,
+                                           redirect_uri=self.redirect_uri,
+                                           client_id=self.client_id,
+                                           client_secret=self.client_secret))
+        data = response.json()
+        LOG.debug(f'Authentication response: {data}')
+        token = data['access_token']
+        return token
--- api/credentials/issuer.py
+++ api/credentials/issuer.py
@@ -7,3 +7,7 @@ class PermissionIssuer(abc.ABC):
     @abc.abstractmethod
     def get_credentials(self, code: str) -> Credentials:
         pass
+
+    @abc.abstractmethod
+    def refresh_token(self, refresh_token) -> str:
+        pass
