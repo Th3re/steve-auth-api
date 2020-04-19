0354f913036bc700aa3ece07e104846b6a7d1943
Mateusz Stompór <mateusz.stompor@ibm.com>
2020-04-20 00:42:58 +0200

    Apply reviewer's suggestions

--- api/api/auth.py
+++ api/api/auth.py
@@ -10,16 +10,15 @@ LOG = logging.getLogger(__name__)
 
 def register_user(scope, code):
     credentials = issuer.get_credentials(code)
-    if credentials:
-        LOG.debug(f'Credentials: {credentials}')
-        manager.save_identity(credentials)
-        return {
-             "code": APICode.OK,
-             "message": "User registered",
-        }, HTTPStatus.OK
-    else:
+    if not credentials:
         LOG.error('Could not issue proper credentials base on the given code')
         return {
-               "code": APICode.ERROR,
-               "message": "User has already granted permissions to the app",
+            "code": APICode.ERROR,
+            "message": "User has already granted permissions to the app",
         }, HTTPStatus.BAD_REQUEST
+    LOG.debug(f'Credentials: {credentials}')
+    manager.save_identity(credentials)
+    return {
+        "code": APICode.OK,
+        "message": "User registered",
+    }, HTTPStatus.OK
